aa68464a6ffcd830038386aac66e4eeed1a27d44
maxrichie5 <maxrichmond@panther.com>
2024-06-20 16:00:48 -0600

    more opinions and examples

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from pypanther import PantherLogType, PantherSeverity, get_panther_rules, register
-from pypanther.rules.aws_cloudtrail_rules.aws_cloudtrail_account_discovery import (
-    AWSCloudTrailAccountDiscovery,
-)
-from pypanther.rules.aws_cloudtrail_rules.aws_cloudtrail_stopped import (
-    AWSCloudTrailStopped,
-)
-from pypanther.rules.aws_cloudtrail_rules.aws_codebuild_made_public import (
-    AWSCloudTrailCodebuildProjectMadePublic,
-)
+from pypanther.rules.aws_cloudtrail_rules.aws_cloudtrail_account_discovery import AWSCloudTrailAccountDiscovery
+from pypanther.rules.aws_cloudtrail_rules.aws_cloudtrail_stopped import AWSCloudTrailStopped
+from pypanther.rules.aws_cloudtrail_rules.aws_codebuild_made_public import AWSCloudTrailCodebuildProjectMadePublic
+from pypanther.rules.aws_cloudtrail_rules.aws_console_login_without_mfa import AWSConsoleLoginWithoutMFA
+from pypanther.rules.aws_cloudtrail_rules.aws_console_login_without_saml import AWSConsoleLoginWithoutSAML
+from pypanther.wrap import exclude, include
 
-from rules.my_first_custom_rule import MyFirstCustomRule
+from rules.aws_cloudtrail_rules.my_first_custom_rule import MyFirstCustomRule
+from rules.aws_cloudtrail_rules.my_first_tuned_rule import MyFirstTunedRule
 
 ########################################################
-# define all variables in one section
+## Variables
 
 onboarded_log_types = [
     # replace with your log types
@@ -20,7 +18,7 @@ onboarded_log_types = [
 ]
 
 ########################################################
-# write all your overrides in one section
+## Overrides
 
 AWSCloudTrailAccountDiscovery.Severity = PantherSeverity.High
 AWSCloudTrailCodebuildProjectMadePublic.DedupPeriodMinutes = 10
@@ -34,17 +32,23 @@ for rule in get_panther_rules(LogTypes=onboarded_log_types, Severity=PantherSeve
     rule.OutputIds.append("Slack #security")
 
 ########################################################
-# register all rules in the last section
+## Filters
 
-# register tells Panther which rules you want uploaded
-# accepts a list of PantherRule's
+include(lambda e: e.get("eventType") == "AwsConsoleSignIn")(AWSConsoleLoginWithoutSAML)
+exclude(lambda e: e.get("awsRegion") == "us-east-2")(AWSConsoleLoginWithoutMFA)
+
+########################################################
+## Register
+
 register(
-    # utility function to easily upload Panther provided rules you care about
-    # you can filter by any rule attribute
     get_panther_rules(
         LogTypes=onboarded_log_types,
     )
 )
 
-# register also accepts individual PantherRule's
-register(MyFirstCustomRule)
+register(
+    [
+        MyFirstCustomRule,
+        MyFirstTunedRule,
+    ]
+)
--- rules/aws_cloudtrail_rules/my_first_custom_rule.py
+++ rules/aws_cloudtrail_rules/my_first_custom_rule.py
@@ -1,11 +1,20 @@
-from pypanther import PantherLogType, PantherRule, PantherSeverity
+from pypanther import PantherLogType, PantherRule, PantherRuleTest, PantherSeverity
 from pypanther.base import PantherEvent
+
+tests = [
+    PantherRuleTest(
+        Name="should be false",
+        ExpectedResult=False,
+        Log={"field": "val"},
+    )
+]
 
 
 class MyFirstCustomRule(PantherRule):
     RuleID = "MyFirstCustomRule"
     LogTypes = [PantherLogType.AWS_CloudTrail]
     Severity = PantherSeverity.Low
+    Tests = tests
 
     def rule(self, event: PantherEvent) -> bool:
         return False
