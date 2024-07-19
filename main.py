d49febcd36bc3427d2c6622d9310a0dc27d31ab0
Jack Naglieri <jack@runpanther.io>
2024-07-19 13:22:14 -0700

    pr feedback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pypanther import LogType, Severity, get_panther_rules, get_rules, register
 from pypanther.get import print_rule_table
-from pypanther.registry import __REGISTRY
+from pypanther.registry import registered_rules
 from pypanther.rules.aws_cloudtrail_rules.aws_cloudtrail_account_discovery import AWSCloudTrailAccountDiscovery
 from pypanther.rules.aws_cloudtrail_rules.aws_cloudtrail_stopped import AWSCloudTrailStopped
 from pypanther.rules.aws_cloudtrail_rules.aws_console_root_login import AWSConsoleRootLogin
@@ -144,4 +144,4 @@ register(
     ]
 )
 
-print_rule_table(__REGISTRY)
+print_rule_table(registered_rules())
