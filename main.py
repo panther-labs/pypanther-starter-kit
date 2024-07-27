e51e95149e136a59240b44299b93f3dc83cacfc6
Jack Naglieri <jack@runpanther.io>
2024-07-27 18:04:29 -0400

    experiment

--- main.py
+++ main.py
@@ -7,90 +7,30 @@ from pypanther.rules.aws_cloudtrail_rules.aws_console_root_login import AWSConso
 from pypanther.wrap import exclude, include
 
 import rules
-from helpers.cloud import account_lookup_by_id, prod_account_ids, update_account_id_tests
+from filters.filters import prod_account_filter, root_login_account_title, guard_duty_sensitive_service_filter
+from helpers.cloud import update_account_id_tests
 from helpers.rule_overrides import get_rule_by_id
 
-########################################################
-## Importing Panther-managed Rules
-##
-## Use get_panther_rules() to return a list of Panther-managed rules.
-## Calling without arguments returns everything and passing attributes
-## like LogTypes, Severity, or Tags filters rules matching the values.
+## Get Rules
 
-# Note: Replace with your onboarded log types
+# Load Panther-managed rules
 onboarded_log_types = [
     LogType.AWS_CLOUDTRAIL,
     LogType.AWS_GUARDDUTY,
     LogType.OKTA_SYSTEM_LOG,
+    LogType.GITHUB_AUDIT,
 ]
-
-# Get Panther-managed rules for onboarded log types
 base_rules = get_panther_rules(log_types=onboarded_log_types)
 
-# Load all rules defined locally in the `rules` module
+# Load rules from the `rules/` folder
 local_rules = get_rules(module=rules)
 
-## Gets all Panther-managed Rules
-# all_rules = get_panther_rules()
-
-## Get Panther-managed rules for specific severities
-# high_sev_rules = get_panther_rules(
-#     log_types=onboarded_log_types,
-#     default_severity=[
-#         Severity.CRITICAL,
-#         Severity.HIGH,
-#     ],
-# )
 
 
-########################################################
-## Filters and Dynamic Functions
-##
-## Filters include/exclude events from being evaluated by rules.
-## Dynamic functions generate alert values based on event data.
-
-
-def prod_account(event):
-    """
-    Uses CloudTrail events to check if an account ID is in the list of production accounts.
-    This uses a variable from the helpers/cloud module.
-    """
-    # TODO() Change this to use event.udm('account_id')
-    return event.get("recipientAccountId") in prod_account_ids
-
-
-sensitive_services = {"s3", "dynamodb", "iam", "secretsmanager", "ec2"}
-
-
-def guard_duty_sensitive_service(event):
-    """Uses GuardDuty findings to check if the event is for a sensitive service."""
-    service_name = event.deep_get("service", "action", "awsApiCallAction", "serviceName")
-    return any(service_name.startswith(service) for service in sensitive_services)
-
-
-def root_login_account(_, event):
-    """
-    Generates a dynamic alert title for root logins using account mappings.
-    Args:
-        _ (self): The PantherRule instance (unused)
-        event (dict): The CloudTrail event
-    """
-    ip_address = event.get("sourceIPAddress")
-    account = account_lookup_by_id(event.get("recipientAccountId"))
-    return f"Root Login from [{ip_address}] in account [{account}]"
-
-
-########################################################
-## Overrides
-##
-## Apply your internal configurations to Panther-managed rules.
-## This can include single attributes, multiple attributes, or filters.
-
-# Override a single rule's severity to Low
-AWSCloudTrailAccountDiscovery.default_severity = Severity.LOW
+## Set Overrides
 
 # Override a set of rule attributes and attach the 'prod_account' filter from above
-include(prod_account)(AWSCloudTrailStopped)
+include(prod_account_filter)(AWSCloudTrailStopped)
 # TODO(panther) This is a temporary workaround for updating AWS account IDs in rule tests
 update_account_id_tests([AWSCloudTrailStopped])
 
@@ -106,13 +46,13 @@ AWSCloudTrailStopped.override(
 
 # Override a title dynamic function to match internal cloud account account mappings
 # Check out helpers/cloud.py for the account_lookup_by_id function
-AWSConsoleRootLogin.title = root_login_account
+AWSConsoleRootLogin.title = root_login_account_title
 
 # Add two filters to all GuardDuty rules
 for rule in base_rules:
     if LogType.AWS_GUARDDUTY in rule.log_types:
         # Include only production accounts
-        include(guard_duty_sensitive_service)(rule)
+        include(guard_duty_sensitive_service_filter)(rule)
         # Exclude any 'Discovery' tactic finding
         exclude(lambda event: event.get("type").startswith("Discovery"))(rule)
 
@@ -120,28 +60,15 @@ for rule in base_rules:
 validate_rule_example = get_rule_by_id(local_rules, "Custom.Validate.MyRule")
 validate_rule_example.allowed_domains = ["example.com"]
 
-########################################################
-## Register
-##
-## Register your rules to upload them to your Panther instance.
-## Register also enables tests to be run with `pypanther test`.
 
-## Register all rules
-# register(all_rules)
+
+## Register Rules
 
 # Register the rules for onboarded log types
 register(base_rules)
 
 # Register all custom rules inside the `rules` module
-# all subpackages of `rules` must have `__init__.py`
 register(local_rules)
 
-register(
-    [
-        AWSCloudTrailStopped,
-        AWSCloudTrailAccountDiscovery,
-        AWSConsoleRootLogin,
-    ]
-)
-
-print_rule_table(registered_rules())
+# Register rules with overrides
+register([AWSCloudTrailStopped, AWSConsoleRootLogin])
