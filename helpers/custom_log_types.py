66aedc5b09039860b4f9f40a949ae2bfc6c8fd64
Ariel Ropek <ariel.ropek@panther.com>
2024-08-07 11:19:36 -0600

    update custom log types example

--- helpers/custom_log_types.py
+++ helpers/custom_log_types.py
@@ -1,3 +1,5 @@
+# Custom log schemas must be present in your Panther instance for upload to work
+# The log type names defined here do not need to include the "Custom." prefix that is automatically added in the Panther UI
 class CustomLogType:
-    HOST_IDS = "Custom.HostIDS"
-    NETWORK_IDS = "Custom.NetworkIDS"
+    HOST_IDS = "HostIDS"
+    NETWORK_IDS = "NetworkIDS"
