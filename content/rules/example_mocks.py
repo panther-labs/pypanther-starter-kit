44825187a06cc4645d4b0672fe2ffa01b1f6c543
Ariel <ariel.ropek@panther.com>
2024-10-08 16:51:41 -0600

    linto

--- content/rules/example_mocks.py
+++ content/rules/example_mocks.py
@@ -1,5 +1,4 @@
-from pypanther import RuleTest, Rule, RuleMock, LogType, Severity
-
+from pypanther import LogType, Rule, RuleMock, RuleTest, Severity
 
 # This is an example of how to use mocks in your tests.
 # The mocks are used to replace the actual object with a mock object.
@@ -8,8 +7,11 @@ from pypanther import RuleTest, Rule, RuleMock, LogType, Severity
 # http://www.ines-panker.com/2020/06/01/python-mock.html
 
 OUTSIDE = "outside"
+
+
 def outside():
     return OUTSIDE
+
 
 class MockTestRule(Rule):
     id = "mock_test_rule"
@@ -19,11 +21,11 @@ class MockTestRule(Rule):
     default_severity = Severity.INFO
     create_alert = False
 
-    VARIABLE = []
+    VARIABLE: list[str] = []
 
     def rule(self, event):
         return self.VARIABLE != []
-    
+
     tests = [
         RuleTest(
             name="Mock new Test",
@@ -32,13 +34,10 @@ class MockTestRule(Rule):
                 RuleMock(
                     # Mock the object VARIABLE with a new object ["test"]
                     object_name="VARIABLE",
-                    new=["test"]
+                    new=["test"],
                 )
             ],
-            log={
-                "action": "Blocked",
-                "internalIp": ""
-            }
+            log={"action": "Blocked", "internalIp": ""},
         ),
         RuleTest(
             name="Mock side_effect Test",
@@ -47,13 +46,10 @@ class MockTestRule(Rule):
                 RuleMock(
                     # Mock the method rule() with a side effect that checks if the action is "Blocked"
                     object_name="rule",
-                    side_effect=lambda e: e.get("action") == "Blocked"
+                    side_effect=lambda e: e.get("action") == "Blocked",
                 )
             ],
-            log={
-                "action": "Blocked",
-                "internalIp": ""
-            }
+            log={"action": "Blocked", "internalIp": ""},
         ),
         RuleTest(
             name="Mock return_value Test",
@@ -62,29 +58,27 @@ class MockTestRule(Rule):
                 RuleMock(
                     # Mock the method rule() with a return value of True
                     object_name="rule",
-                    return_value=True
+                    return_value=True,
                 )
             ],
-            log={
-                "action": "Blocked",
-                "internalIp": ""
-            }
+            log={"action": "Blocked", "internalIp": ""},
         ),
     ]
 
 
-    
 class OutsideMockTest(Rule):
     id = "outside_mock_test"
     display_name = "Outside Mock Test"
-    default_description = "This rule provides an example of how to mock objects outside of the rule class in rule tests."
+    default_description = (
+        "This rule provides an example of how to mock objects outside of the rule class in rule tests."
+    )
     log_types = [LogType.AZURE_AUDIT]
     default_severity = Severity.INFO
     create_alert = False
 
     def rule(self, event):
         return outside() == "outside"
-    
+
     tests = [
         RuleTest(
             name="Mock outside new",
@@ -93,13 +87,10 @@ class OutsideMockTest(Rule):
                 RuleMock(
                     # Mock the object OUTSIDE with a new value "inside"
                     object_name="OUTSIDE",
-                    new="inside"
+                    new="inside",
                 )
             ],
-            log={
-                "action": "Blocked",
-                "internalIp": ""
-            }
+            log={"action": "Blocked", "internalIp": ""},
         ),
         RuleTest(
             name="Mock outside return_value",
@@ -108,13 +99,10 @@ class OutsideMockTest(Rule):
                 RuleMock(
                     # Mock the function outside() with a return value "inside"
                     object_name="outside",
-                    return_value="inside"
+                    return_value="inside",
                 )
             ],
-            log={
-                "action": "Blocked",
-                "internalIp": ""
-            }
+            log={"action": "Blocked", "internalIp": ""},
         ),
         RuleTest(
             name="Mock outside side_effect",
@@ -123,12 +111,9 @@ class OutsideMockTest(Rule):
                 RuleMock(
                     # Mock the function outside() with a side effect that returns "inside"
                     object_name="outside",
-                    side_effect=lambda: "inside"
+                    side_effect=lambda: "inside",
                 )
             ],
-            log={
-                "action": "Blocked",
-                "internalIp": ""
-            }
+            log={"action": "Blocked", "internalIp": ""},
         ),
     ]
